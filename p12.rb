91ef03541925b0ce948e58e7f9e3d4eca97ccb19
Tenji Tembo <us74092@umbc.edu>
2014-06-06 16:55:07 -0700

    Added Some Comments
    Commenting what’s been done so far.

--- p12.rb
+++ p12.rb
@@ -1,6 +1,8 @@
 class P12
+  #Base Alphabet used for Decryption
   ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+  #Performs the Rotation of an array by the shift amount
   def self.rotate(example_array, num = 1)
     num.times do
       example_array.push(example_array.shift)
@@ -9,6 +11,9 @@ class P12
     example_array
   end
 
+  #Performs the Alphabet Rotation
+    #takes the standard alphabet and rotates it until it is 
+    #the cypher Alphabet we need
   def self.get_alphabet(shift = 0)
     alphabet_array = ALPHABET.split("")
     new_alphabet = rotate(alphabet_array, shift).join
@@ -16,6 +21,11 @@ class P12
     return new_alphabet
   end
 
+  #Performs the Word decryption based on the passed Cypher Alphabet
+    #takes in the cypher alphabet and the word
+    #splits the word and the ca to arrays
+    #determines the location of the encrypted letter and returns
+    #the correct letter
   def self.decrypt_word(encrypted_word, cypher_alphabet)
     new_word = []
     ca_array = cypher_alphabet.split("")
